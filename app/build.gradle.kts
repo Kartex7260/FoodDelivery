d9035d04f17052d1c0a268ffa2a943432b646b6e
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-03 12:29:21 +0300

    Настройка проекта на UI

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+	id("com.android.application")
+	id("org.jetbrains.kotlin.android")
+}
+
+android {
+	namespace = "kanti.fooddelivery"
+	compileSdk = 34
+
+	defaultConfig {
+		applicationId = "kanti.fooddelivery"
+		minSdk = 24
+		targetSdk = 34
+		versionCode = 1
+		versionName = "1.0"
+
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
+
+	buildTypes {
+		release {
+			isMinifyEnabled = false
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
+		}
+	}
+	compileOptions {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
+	}
+	kotlinOptions {
+		jvmTarget = "1.8"
+	}
+	buildFeatures {
+		viewBinding = true
+	}
+}
+
+dependencies {
+
+	testImplementation("junit:junit:4.13.2")
+	androidTestImplementation("androidx.test.ext:junit:1.1.5")
+	androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+//	Ui
+	implementation("androidx.core:core-ktx:1.9.0")
+	implementation("androidx.appcompat:appcompat:1.6.1")
+	implementation("com.google.android.material:material:1.10.0")
+	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+	implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+	implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+	implementation("android.arch.navigation:navigation-ui-ktx:1.0.0")
+
+//	Data
+
+}

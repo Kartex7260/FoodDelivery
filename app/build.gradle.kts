4ec56c7e4f7b3b97e5337b4c21db6c043d97956c
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-03 14:16:42 +0300

    FEAT: вёрстка основного активити

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,12 +46,13 @@ dependencies {
 
 //	Ui
 	implementation("androidx.core:core-ktx:1.9.0")
+	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 	implementation("androidx.appcompat:appcompat:1.6.1")
 	implementation("com.google.android.material:material:1.10.0")
 	implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 	implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
 	implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-	implementation("android.arch.navigation:navigation-ui-ktx:1.0.0")
+	implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
 //	Data
 
---
+++ app/src/main/java/kanti/fooddelivery/ui/fragments/screens/FoodListFragment.kt
@@ -0,0 +1,26 @@
+package kanti.fooddelivery.ui.fragments.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kanti.fooddelivery.databinding.FragmentFoodListBinding
+
+class FoodListFragment : Fragment() {
+
+	private var _viewBinding: FragmentFoodListBinding? = null
+	private val viewBinding: FragmentFoodListBinding get() = _viewBinding!!
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		if (_viewBinding == null) {
+			_viewBinding = FragmentFoodListBinding.inflate(inflater, container, false)
+		}
+		return viewBinding.root
+	}
+
+}

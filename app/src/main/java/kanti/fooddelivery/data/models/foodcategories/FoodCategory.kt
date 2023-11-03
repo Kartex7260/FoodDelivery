0978176ebe19e976e7a3adc0c14a2a9156457a5c
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-04 00:14:44 +0300

    BREAKING CHANGE: более правильное имя для FoodType.kt

---
+++ app/src/main/java/kanti/fooddelivery/data/models/foodcategories/FoodCategory.kt
@@ -0,0 +1,6 @@
+package kanti.fooddelivery.data.models.foodcategories
+
+data class FoodCategory(
+	val id: Int,
+	val name: String
+)
--- app/src/main/java/kanti/fooddelivery/data/models/foodtype/FoodType.kt
+++
@@ -1,6 +0,0 @@
-package kanti.fooddelivery.data.models.foodtype
-
-data class FoodType(
-	val id: Int,
-	val name: String
-)

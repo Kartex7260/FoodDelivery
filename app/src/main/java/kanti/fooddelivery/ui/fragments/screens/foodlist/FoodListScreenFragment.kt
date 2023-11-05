34852222ececeb88a29f91ccd290a7366f5de0f6
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-05 19:02:18 +0300

    FEAT: скрытие плашки с акциями во время скролинга

--- app/src/main/java/kanti/fooddelivery/ui/fragments/screens/foodlist/FoodListScreenFragment.kt
+++ app/src/main/java/kanti/fooddelivery/ui/fragments/screens/foodlist/FoodListScreenFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kanti.fooddelivery.data.models.discounts.Discount
+import kanti.fooddelivery.data.models.food.Food
 import kanti.fooddelivery.databinding.FragmentFoodListBinding
+import kanti.fooddelivery.ui.view.foodcategories.FoodCategoryChip
+import kanti.fooddelivery.ui.view.recycleradapters.DiscountRecyclerAdapter
+import kanti.fooddelivery.ui.view.recycleradapters.FoodItemRecyclerAdapter
 
 class FoodListScreenFragment : Fragment() {
 
@@ -27,8 +32,84 @@ class FoodListScreenFragment : Fragment() {
 		super.onViewCreated(view, savedInstanceState)
 		viewBinding.apply {
 			for (i in 0..4) {
-
+				val chip = FoodCategoryChip.create(requireContext()).apply {
+					text = "Test text"
+				}
+				if (i == 0) {
+					chip.isChecked = true
+				}
+				foodCategoriesChipGroup.addView(chip)
 			}
+
+			foodListRecyclerView.adapter = FoodItemRecyclerAdapter(
+				listOf(
+					Food(
+						title = "Pizza",
+						composition = "Тесто, мука, яблоки, агуша",
+						minPrice = 350,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "Роллы",
+						composition = "ЛА ла  ла ллла фва ы  впь лыоууоыаоы олоуал оылуоало лыуа",
+						minPrice = 50000,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "Филадельфия",
+						composition = "Лл фыл ллыа флыалл лфыылалфыл флыал лфыа лыфлл алфылал лфыла лфыла ",
+						minPrice = -2,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "Салями",
+						composition = "ЛАлвлф вллыв узу й9к893 9 гк9фгу к",
+						minPrice = 350,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "",
+						composition = "",
+						minPrice = 350,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "",
+						composition = "",
+						minPrice = 350,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "",
+						composition = "",
+						minPrice = 350,
+						imageUrl = "",
+						foodType = 0
+					),
+					Food(
+						title = "",
+						composition = "",
+						minPrice = 350,
+						imageUrl = "",
+						foodType = 0
+					)
+				)
+			) { _, _ -> }
+
+			discountsRecyclerView.adapter = DiscountRecyclerAdapter(
+				listOf(
+					Discount(""),
+					Discount(""),
+					Discount(""),
+					Discount("")
+				)
+			) { _, _ -> }
 		}
 	}
 

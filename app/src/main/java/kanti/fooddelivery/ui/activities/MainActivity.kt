51eb0e59d2cdd3001c90c2070a46c5e88cb42342
Kirill Zhitko <kirillzhitko@gmail.com>
2023-11-06 16:57:15 +0300

    FEAT: Добавление ErrorCard на MainActivity

--- app/src/main/java/kanti/fooddelivery/ui/activities/MainActivity.kt
+++ app/src/main/java/kanti/fooddelivery/ui/activities/MainActivity.kt
@@ -10,15 +10,21 @@ import com.google.android.material.progressindicator.CircularProgressIndicator
 import dagger.hilt.android.AndroidEntryPoint
 import kanti.fooddelivery.R
 import kanti.fooddelivery.databinding.ActivityMainBinding
+import kanti.fooddelivery.ui.common.errorprovider.ErrorProvider
+import kanti.fooddelivery.ui.common.errorprovider.ErrorProviderOwner
 import kanti.fooddelivery.ui.common.progressindicatorowner.CircularProgressIndicatorOwner
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), CircularProgressIndicatorOwner {
+class MainActivity : AppCompatActivity(), CircularProgressIndicatorOwner, ErrorProviderOwner {
 
 	private lateinit var viewBinding: ActivityMainBinding
 
 	override val circularProgressIndicator: CircularProgressIndicator
 		get() = viewBinding.progressView
+
+	override var errorProvider: ErrorProvider?
+		get() = TODO("Not yet implemented")
+		set(value) {}
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
